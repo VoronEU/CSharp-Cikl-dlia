abaa209685579263c3be0eaa37b24af76d6fe82f
VoronEU <93764594+VoronEU@users.noreply.github.com>
2025-12-15 21:48:07 +0600

    Implement sorting of three integers in ascending order

---
+++ Task3_SortAscending.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
+        if (a > b) { int t = a; a = b; b = t; }
+        if (b > c) { int t = b; b = c; c = t; }
+        if (a > b) { int t = a; a = b; b = t; }
+
+        Console.WriteLine($"{a} {b} {c}");
+    }
+}
